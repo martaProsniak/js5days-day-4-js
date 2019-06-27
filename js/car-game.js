193b4eac249cfc923368222c78824858817f119f
martaprosniak <martaprosniak@gmail.com>
2019-06-27 11:09:04 +0200

    Car game encapsulated with wrapper function

--- js/car-game.js
+++ js/car-game.js
@@ -1,68 +1,70 @@
-const containerElement = document.querySelector('.car-game')
-const carElement = document.createElement('div')
-const roadElement = document.createElement('div')
+function carGame(selector) {
 
-const tickDuration = 10
-const maxAcceleration = 10
+    const containerElement = document.querySelector(selector)
+    const carElement = document.createElement('div')
+    const roadElement = document.createElement('div')
 
-let velocity = 0
-let acceleration = 0
-let position = 0
+    const tickDuration = 10
+    const maxAcceleration = 10
 
-function displayCarAndCar() {
-    carElement.style.width = '100px'
-    carElement.style.height = '100px'
-    carElement.style.backgroundColor = 'red'
+    let velocity = 0
+    let acceleration = 0
+    let position = 0
 
-    carElement.style.position = 'absolute'
-    carElement.style.left = position + 'px'
+    function displayCarAndCar() {
+        carElement.style.width = '100px'
+        carElement.style.height = '100px'
+        carElement.style.backgroundColor = 'red'
 
-    roadElement.style.backgroundColor = 'grey'
-    roadElement.style.width = '100%'
-    roadElement.style.height = '100px'
-    roadElement.style.position = 'relative'
-    
-    roadElement.appendChild(carElement)
-    containerElement.appendChild(roadElement)
-}
+        carElement.style.position = 'absolute'
+        carElement.style.left = position + 'px'
 
-function move() {
-    const time = tickDuration / 1000
+        roadElement.style.backgroundColor = 'grey'
+        roadElement.style.width = '100%'
+        roadElement.style.height = '100px'
+        roadElement.style.position = 'relative'
 
-    position = position + velocity * time + ((acceleration*time*time) / 2)
-    velocity = velocity + acceleration * time
-    carElement.style.left = position + 'px'
-}
-
-displayCarAndCar();
-
-setInterval(
-    // move instead of move() to pass the whole function into setInterval function
-    move,
-    tickDuration
-)
-
-window.addEventListener(
-    'keydown',
-    function (event) {
-        if (event.key === 'a') {
-            acceleration = maxAcceleration
-        }
-        if (event.key === 'b') {
-            acceleration = -maxAcceleration
-        }
+        roadElement.appendChild(carElement)
+        containerElement.appendChild(roadElement)
     }
-)
 
-window.addEventListener(
-    'keyup',
-    function (event) {
-        if (event.key === 'a') {
-            acceleration = 0
-        }
-        if (event.key === 'b') {
-            acceleration = 0
-        }
+    function move() {
+        const time = tickDuration / 1000
+
+        position = position + velocity * time + ((acceleration * time * time) / 2)
+        velocity = velocity + acceleration * time
+        carElement.style.left = position + 'px'
     }
-)
 
+    displayCarAndCar();
+
+    setInterval(
+        // move instead of move() to pass the whole function into setInterval function
+        move,
+        tickDuration
+    )
+
+    window.addEventListener(
+        'keydown',
+        function (event) {
+            if (event.key === 'a') {
+                acceleration = maxAcceleration
+            }
+            if (event.key === 'b') {
+                acceleration = -maxAcceleration
+            }
+        }
+    )
+
+    window.addEventListener(
+        'keyup',
+        function (event) {
+            if (event.key === 'a') {
+                acceleration = 0
+            }
+            if (event.key === 'b') {
+                acceleration = 0
+            }
+        }
+    )
+}
